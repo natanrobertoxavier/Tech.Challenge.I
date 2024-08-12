d3c2d0ee89512812d1b156a31471ab51e166abe9
Natan Roberto Xavier <natanxavier@policiamilitar.sp.gov.br>
2024-08-12 17:12:17 -0300

    add testes unitarios registerddd e register usuarios

---
+++ Tech.Challenge.I/Tech.Challenge.I.Tests/Validators/RegisterRegionDDDValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentValidation.TestHelper;
+using Tech.Challenge.I.Application.UseCase.DDD.Register;
+using Tech.Challenge.I.Communication.Request;
+using Tech.Challenge.I.Communication.Request.Enum;
+using Tech.Challenge.I.Exceptions;
+
+namespace Tech.Challenge.I.Tests.Validators;
+public class RegisterRegionDDDValidatorTests
+{
+    private readonly RegisterRegionDDDValidator _validator;
+
+    public RegisterRegionDDDValidatorTests()
+    {
+        _validator = new RegisterRegionDDDValidator();
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_RequestIsValid()
+    {
+        // Arrange
+        var model = new RequestRegionDDDJson { Region = RegionRequestEnum.Sudeste, DDD = 21 };
+
+        var validator = new RegisterRegionDDDValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_DDDIsOutOfRange()
+    {
+        // Arrange
+        var model = new RequestRegionDDDJson { Region = RegionRequestEnum.Sudeste, DDD = 100 };
+
+        var validator = new RegisterRegionDDDValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.DDDBetweenTenNinetyNine, result.Errors.Select(e => e.ErrorMessage));
+    }
+
+    [Fact]
+    public void Should_HaveError_When_DDDIsEmpty()
+    {
+        // Arrange
+        var model = new RequestRegionDDDJson { Region = RegionRequestEnum.Norte, DDD = 0 };
+
+        var validator = new RegisterRegionDDDValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.DDDNotFound, result.Errors.Select(e => e.ErrorMessage));
+    }
+
+    [Fact]
+    public void Should_HaveError_When_RegionIsInvalid()
+    {
+        // Arrange
+        var model = new RequestRegionDDDJson { Region = (RegionRequestEnum)999, DDD = 21 };
+
+        var validator = new RegisterRegionDDDValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.InvalidRegion, result.Errors.Select(e => e.ErrorMessage));
+    }
+}
---
+++ Tech.Challenge.I/Tech.Challenge.I.Tests/Validators/RegisterUserValidatorTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentValidation.TestHelper;
+using Tech.Challenge.I.Application.UseCase.User.Register;
+using Tech.Challenge.I.Communication.Request;
+using Tech.Challenge.I.Exceptions;
+
+namespace Tech.Challenge.I.Tests.Validators;
+public class RegisterUserValidatorTests
+{
+    private readonly RegisterUserValidator _validator;
+
+    public RegisterUserValidatorTests()
+    {
+        _validator = new RegisterUserValidator();
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_RequestIsValid()
+    {
+        // Arrange
+        var model = new RequestRegisterUserJson { Name = "John Doe", Email = "user@example.com", Password = "ValidPass123" };
+
+        var validator = new RegisterUserValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_PasswordIsInvalid()
+    {
+        // Arrange
+        var model = new RequestRegisterUserJson { Name = "John Doe", Email = "user@example.com", Password = "short" };
+
+        var validator = new RegisterUserValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.MinimumSixCharacters, result.Errors.Select(e => e.ErrorMessage));
+    }
+
+    [Fact]
+    public void Should_HaveError_When_EmailIsInvalid()
+    {
+        // Arrange
+        var model = new RequestRegisterUserJson { Name = "John Doe", Email = "invalid-email", Password = "ValidPass123" };
+
+        var validator = new RegisterUserValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.InvalidUserEmail, result.Errors.Select(e => e.ErrorMessage));
+    }
+
+    [Fact]
+    public void Should_HaveError_When_EmailIsEmpty()
+    {
+        // Arrange
+        var model = new RequestRegisterUserJson { Name = "John Doe", Email = string.Empty, Password = "ValidPass123" };
+
+        var validator = new RegisterUserValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.BlankUserEmail, result.Errors.Select(e => e.ErrorMessage));
+    }
+
+    [Fact]
+    public void Should_HaveError_When_NameIsEmpty()
+    {
+        // Arrange
+        var model = new RequestRegisterUserJson { Name = string.Empty, Email = "user@example.com", Password = "ValidPass123" };
+
+        var validator = new RegisterUserValidator();
+
+        // Act
+        var result = validator.TestValidate(model);
+
+        // Assert
+        Assert.Contains(ErrorsMessages.BlankUserName, result.Errors.Select(e => e.ErrorMessage));
+    }
+}
